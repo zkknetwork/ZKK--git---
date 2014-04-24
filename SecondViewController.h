06b6e983f7c18b1625d389c24cac2c3a90da26e5
zkkgithub <zoro.zhang@deyiff.com>
2014-04-24 09:37:34 +0800

    //.新增文件

---
+++ SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ZKK测试git的使用
+//
+//  Created by zorro on 14-4-24.
+//  Copyright (c) 2014年 deyiff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ SecondViewController.m
@@ -0,0 +1,56 @@
+//
+//  SecondViewController.m
+//  ZKK测试git的使用
+//
+//  Created by zorro on 14-4-24.
+//  Copyright (c) 2014年 deyiff. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    // 新建的第二个文件
+    
+    for (int i = 0; i < 1; i++)
+    {
+        NSLog(@"..%@", [UIDevice currentDevice].localizedModel);
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
